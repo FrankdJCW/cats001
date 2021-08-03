4aa812f7e1e050e3357284bcbfa0ec43ce49778c
FrankdJCW <frankcontreras315@gmail.com>
2021-08-03 15:59:19 -0400

    commit 2

---
+++ lib/pages/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      if (account != null) {
+        print('User Signed In $account');
+        setState(() {
+          isAuth = true;
+        });
+      } else {
+        setState(() {
+          isAuth = false;
+        });
+      }
+    }, onError: (err) {
+      print('Error signing in: $err');
+    });
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                  Theme.of(context).accentColor,
+                  Theme.of(context).primaryColor
+                ])),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'FIU Animals',
+                  style: TextStyle(
+                    fontFamily: "Signatra",
+                    fontSize: 90.0,
+                    color: Colors.white,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: login,
+                  child: Container(
+                    width: 260.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/google_signin_button.png'),
+                            fit: BoxFit.cover)),
+                  ),
+                )
+              ],
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
